400fe7db77ac9ba530f2b0731385ee3fc77a4722
Sven Groot <sgroot@gmail.com>
2022-12-28 12:43:35 -0800

    Consolidate Linux build scripts.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,43 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-mkdir -p $DIR/../out/lx
-pushd $DIR/../out/lx
-cmake ../..
-cmake --build . --parallel 8
+TARGET_DIR="$DIR/../out/lx"
+while [[ $# -gt 0 ]]; do
+	case $1 in
+	-c|--compiler)
+		CXX=$(which $2)
+		TARGET_DIR="$DIR/../out/$2"
+		shift
+		shift
+		;;
+	-t|--test)
+		TEST=YES
+		shift
+		;;
+	-d|--docs)
+		DOCS=YES
+		shift
+		;;
+	--clean)
+		rm -rf $TARGET_DIR
+		shift
+		;;
+	*)
+		echo "Unknown argument $1"
+		exit 1
+		;;
+	esac
+done
 
-if [ "$1" == "test" ]; then
+mkdir -p $TARGET_DIR
+pushd $TARGET_DIR > /dev/null
+CXX=$CXX cmake ../..
+if [ "$DOCS" == "YES" ]; then
+	cmake --build . --target docs
+else
+	cmake --build . --parallel 8
+fi
+
+if [ "$TEST" == "YES" ]; then
 	ctest . --output-on-failure
 fi
-popd
+popd > /dev/null
--- scripts/build_docs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-mkdir -p $DIR/../out/lx
-pushd $DIR/../out/lx
-cmake ../..
-cmake --build . --target docs
-popd
